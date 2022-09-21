93bc7e12e21e17dd6a5059c47d93bdbca94c8345
m-lafauci <mglafauci@gmail.com>
2022-09-20 22:54:15 -0300

    Counter

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Title from '../Title';
+import ItemCount from "../ItemCount";
 
 export const ItemListContainer = ({ texto }) => {
+
+    const onAdd = (quantity) => {
+        console.log(`compraste ${quantity} unidades`)
+    }
+
     return(
-        <Title greeting={texto} />        
+        <>
+        <Title greeting={texto} />
+        <ItemCount initial={3} stock={5} onAdd={onAdd} />
+        </>
+
     );
 }
 
