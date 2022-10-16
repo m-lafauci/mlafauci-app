e38b0ed254c18ae52d768fd962225c06af7ce975
m-lafauci <mglafauci@gmail.com>
2022-10-16 10:12:40 -0300

    Proyecto Final

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 import Form from '../Form';
+import { useCartContext } from "../../context/CartContext";
 
 const Checkout = () => {
+    const  { clearCart } = useCartContext();
+
     const [orderId, setOrderId] = useState('');
 
     const handleId = (numeroDeOrden) => {
@@ -14,6 +17,7 @@ const Checkout = () => {
             <div>
                 <img src="../images/thanks.png" alt="thank you" />
                 <h4>ID de Pedido:{' '}<h2><span className="badge rounded-pill">{orderId}</span></h2></h4>
+                {clearCart()}
             </div>
         );
     }
